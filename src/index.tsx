d59a5ac62b4317b4aa735c6ac05667334ac92162
AlexForw <lmityushkin@mail.ru>
2022-06-11 23:42:23 +0300

    add layout in app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import App from './App';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
